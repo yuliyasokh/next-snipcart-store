70ed5f2448811867225b713600a3ae1f502a7225
yuliya.sokh@gmail.com <21014151y>
2021-04-02 19:33:41 +0200

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import products from '../products.json';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Link } from 'react-router-dom'
+import NumericInput from 'react-numeric-input';
 
 export default function Home() {
   return (
@@ -8,46 +14,58 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+        <nav className="nav-wrapper">
+            <div className="container">
+                <Link to="/" className="brand-logo">Shopping</Link>
 
-      <main className={styles.main}>
+                <ul className="right">
+                    <li><Link to="/">Shop</Link></li>
+                    <li><Link to="/cart">My cart</Link></li>
+                    <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
+                </ul>
+            </div>
+        </nav>
+        <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+            Pelmeni Pierogi Vareniki
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+            Только из 100% натуральных продуктов, ручная лепка!
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <div className={styles.grid}>
+              {products.map(product => {
+                  return (
+                      <div key={product.id} className={styles.card}>
+                          <img src={product.image} alt={`Preview of ${product.title}`} />
+                          <h3>{ product.title }</h3>
+                          <p>{ product.ingredients }</p>
+                          <p></p>
+                          <p>{ product.description }</p>
+                          <p className={styles.ldescription}>zł { product.price } / кг</p>
+                          <p><div className = {styles.quantity}>
+                              <NumericInput className="form-control"
+                                  value={ 1.0 }
+                                  min={ 0.5 }
+                                  max={ 100 }
+                                  step={ 0.5 }
+                                  precision={ 1 }
+                                  size={ 5 }
+                                  mobile
+                              />
+                              <button className="snipcart-add-item"
+                                      data-item-id={product.id}
+                                      data-item-image={product.image}
+                                      data-item-name={product.title}
+                                      data-item-url="/"
+                                      data-item-price={product.price}
+                              >Добавить в корзину</button>
+                          </div>
+                          </p>
+                      </div>
+                  );
+              })}
+          </div>
       </main>
 
       <footer className={styles.footer}>
@@ -56,9 +74,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Made by yuliya.sokh
         </a>
+          <script async src="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.js" />
+          <div hidden id="snipcart" data-api-key="[YOUR-API-KEY]" />
       </footer>
     </div>
   )
